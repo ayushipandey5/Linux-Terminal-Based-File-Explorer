7a7051ab3eb3b8ec2c9474a7bd8d77ea3fe2ef1c
Ayushi Pandey <41800690+ayushipandey5@users.noreply.github.com>
2020-10-08 21:26:44 +0530

    Update main.cpp

--- AOS_Assignment_1/main.cpp
+++ AOS_Assignment_1/main.cpp
@@ -1,1 +1,40 @@
-#include<bits/stdc++.h>
+#include "headers.h"
+// #include"DirListing.h"
+// #include"keyMovement.h"
+// #include"commandExecution.h"
+
+
+string root;
+
+int main(int argc, char ** argv){
+    cout<<"\u001b[2J";
+    if(argc == 1){
+        string current=".";
+        dirList = addDir(current);
+        if(chdir(current.c_str())<0)
+            cout<<"Error"<<endl;
+        low=0;high=25;
+        disDir(low,high,dirList);
+
+        char pathBuff[200];
+        string path = getcwd(pathBuff,200);
+        root = path;
+        int curr_ptr=0;
+        normalMode(&curr_ptr);
+    }
+
+    if(argc>=2){
+        string current=argv[1];
+        dirList = addDir(current);
+        if(chdir(current.c_str())<0)
+            cout<<"Error"<<endl;
+        disDir(low,high,dirList);
+
+        char pathBuff[200];
+        string path = getcwd(pathBuff,200);
+        root = path;
+        commandMode();
+    }
+    
+}
+
