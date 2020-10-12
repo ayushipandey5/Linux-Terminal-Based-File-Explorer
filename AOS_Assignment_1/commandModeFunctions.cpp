e4c30d0aa0d42c0f7f7aeaa14ab9d9fa82bf8199
Ayushi Pandey <41800690+ayushipandey5@users.noreply.github.com>
2020-10-13 03:33:29 +0530

    Update commandModeFunctions.cpp

--- AOS_Assignment_1/commandModeFunctions.cpp
+++ AOS_Assignment_1/commandModeFunctions.cpp
@@ -149,3 +149,92 @@ string createfile(string src)
 	ftsrc.close();
 	return mesg;
 }
+
+
+string deletefile(string src)
+{   
+    string mesg; 
+    if( remove(src.c_str()) != 0 )
+        mesg="Error deleting file" ;
+    else
+        mesg= "File successfully deleted" ;
+	return mesg;                                        
+}
+
+string delete_dir(string src)
+{
+    string mesg;
+    if (rmdir(src.c_str()) != 0)
+        mesg= "Error deleting folder";
+    else
+        mesg= "Folder successfully deleted" ;
+    return mesg;                        
+}
+
+vector<string> searchFile(string dir,vector<string>&search1,string file)
+{
+	DIR *dp;
+	struct dirent *entry;
+	struct stat statbuf;
+	char buffer[256];
+	string  basePath = getcwd(buffer, 256);
+	if((dp = opendir(dir.c_str())) == NULL) 
+	{
+	//return search1;
+	}
+	chdir(dir.c_str());
+	while((entry = readdir(dp)) != NULL) 
+	{
+		lstat(entry->d_name,&statbuf);
+		if(S_ISDIR(statbuf.st_mode)) 
+		{
+		if(strcmp(".",entry->d_name) == 0 ||
+		strcmp("..",entry->d_name) == 0)
+		continue;
+		search1=searchFile(dir+"/"+string((entry->d_name)),search1,file);
+	    }
+	    else 
+		{
+			if(string(entry->d_name)==file)
+			//search1.push_back(dir);
+			///////////////////////*
+				{vector<string> tokens;
+					
+					stringstream check1(dir);
+					string rawcode;
+					while(getline(check1, rawcode, '/'))
+					{
+						tokens.push_back(rawcode);
+					}	
+					search1.push_back(tokens[tokens.size()-1]);}
+			//////////////////
+			//search1.push_back(dir+"/"+string(entry->d_name));
+		
+		}
+	}
+	chdir(basePath.c_str());
+	closedir(dp);
+	return search1;
+}
+
+
+void displaySearchResult(int low,int high,vector<string>DirectryList)
+{	
+	cout<<"\033c";//clear screen
+	char buffer[256];
+	string path = getcwd(buffer, 256);
+	cout<<path<<endl<<endl;
+	int i;
+	if(DirectryList.size()<25)
+	high=DirectryList.size();
+	cout<<"\u001b[33m";
+	for(i=low;i<high;i++)
+	{
+	cout<<DirectryList[i]<<endl;
+	
+	}
+	cout<<"\u001b[0m";
+	if(high<22)
+	cout<<"\u001b["<<high-low+2<<"A";
+	
+}
