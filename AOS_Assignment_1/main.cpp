4895afaff7669aaea508cee28a9c511bb3e4c1f0
Ayushi Pandey <41800690+ayushipandey5@users.noreply.github.com>
2020-10-12 22:21:35 +0530

    Update main.cpp

--- AOS_Assignment_1/main.cpp
+++ AOS_Assignment_1/main.cpp
@@ -1,32 +1,12 @@
 #include "headers.h"
-// #include"DirListing.h"
-// #include"keyMovement.h"
-// #include"commandExecution.h"
 
+string * root;
 
-string root;
-
+string terminalMode="NORMAL";
 int main(int argc, char ** argv){
-    cout<<"\u001b[2J";
-    if(argc == 1){
-        string current=".";
-        dirList = addDir(current);
-        if(chdir(current.c_str())<0)
-            cout<<"Error"<<endl;
-        struct winsize w;
-        ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-        int low = 1 ;
-        int high = w.ws_row - 2;
-
-        disDir(low,high,dirList);
-
-        char pathBuff[200];
-        string path = getcwd(pathBuff,200);
-        root = path;
-        int curr_ptr=0;
-        normalMode(&curr_ptr);
-    }
-
+    cout<<"\u001b[2J"; //clear screen
+    cout<<"\033[2;1H";
+    vector<string>dirList;
     if(argc>=2){
         string current=argv[1];
         dirList = addDir(current);
@@ -42,9 +22,27 @@ int main(int argc, char ** argv){
 
         char pathBuff[200];
         string path = getcwd(pathBuff,200);
-        root = path;
-        commandMode();
+        *root = path;
+        //commandMode();
     }
+    else if(argc < 2){   
+        int low = 0 ;
+        int high = getWinRow() - 1;
+
+        char pathBuff[256];
+        string path = getcwd(pathBuff,256);
+        root = &path;
+        dirList = addDir(".");
+
+        for(int i=0;i<dirList.size();i++){
+            cout<<dirList[i]<<endl;
+        }
+        disDir(low,high,dirList);
+
+        int curr_ptr=0;
+        normalMode(&curr_ptr,dirList);
+    }
+
     
 }
 
